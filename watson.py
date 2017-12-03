cf571a9368442b44de82a26a72d6f4c6fc89fb11
Julia McClellan <juliamcclellan37@gmail.com>
2017-12-03 07:28:44 -0500

    Create watson.py

---
+++ watson.py
@@ -0,0 +1,16 @@
+import os
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from glob import glob
+
+tone_analyzer = ToneAnalyzerV3(
+  version='2016-05-19',
+  username='aa868ff9-28a0-4ec7-a7e5-d41a8a1198a0',
+  password='LU0EOlno2TNq'
+)
+
+def analyze(text):
+    tone = tone_analyzer.tone(text, tones='emotion', content_type='text/plain')
+    print(json.dumps(tone, indent=2))
+
+analyze("the weather has been so dreary lately")
