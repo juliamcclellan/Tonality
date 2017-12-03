14132f44a68e6c481b3d8d8976060933b6f8dd74
Julia McClellan <juliamcclellan37@gmail.com>
2017-12-03 07:38:01 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from file_processor import open_file
+from audio_parse import parse_audio
+from watson import analyze
+
+audio = open_file('resources', 'audio.raw')
+parsed = parse_audio(audio)
+
+for result in parsed:
+    tone = analyze(result.alternatives[0].transcript)
+    for line in tone:
+        print(line)
